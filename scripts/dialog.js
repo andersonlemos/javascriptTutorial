bd5361e0cb9b3611ce5e89a1b8f786a5a600ee54
Anderson Lemos <andersonlemos.net@gmail.com>
2014-04-14 23:11:19 -0300

    test files

---
+++ scripts/dialog.js
@@ -0,0 +1,11 @@
+/**
+ * Created by root on 14/04/14.
+ */
+function setPrompt(){
+
+  var data= prompt("Olá visitante\n digite a data do seu nascimento.","dd/mm/aaaa");
+
+    window.document.write("<p> o nascimento é " + data +" </p>");
+
+}
+
